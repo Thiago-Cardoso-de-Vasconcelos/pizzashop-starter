f067e503e9e616275d5b7183689a30011b1e3ef8
Thiago Vasconcelos <thiagocardosodevasconcelos@outlook.com>
2024-03-08 19:01:39 -0300

    feat: add cartBottom

--- client/src/app/components/CartBottom.jsx
+++ client/src/app/components/CartBottom.jsx
@@ -1,5 +1,39 @@
+import React, { useContext, useState } from 'react';
+//icons
+import { IoCloseOutline } from 'react-icons/io5';
+//components
+import CheckoutDetails from './CheckoutDetails';
+//modal
+import Modal from 'react-modal';
+//context
+import { CartContext } from '../context/CartContext';
+
 const CartBottom = () => {
-  return <div>CartBottom</div>;
+  const { setIsOpen, cart } = useContext(CartContext);
+
+  return (
+    <>
+      {cart.length >= 1 ? (
+        <div className='px-6 py-3 lg:py-6 mt-auto'>
+          {/* total price  */}
+          <div className='flex items-center justify-between mb-6 text-lg font-semibold font-robotoCondensed'>
+            <div>Total:</div>
+            <div>$320</div>
+          </div>
+          {/* btn  */}
+          <div className='flex  flex-col gap-y-3'>
+          <button className='btn btn-lg gradient fonr-font-semibold flex justify-center'>
+            Checkout
+          </button>
+          </div>
+        </div>
+      ) : (
+        <div className='absolute top-0 w-full h-full flex justify-center items-center -z-10'>
+        <div className='font-semibold'>Your cart is empty</div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CartBottom;
